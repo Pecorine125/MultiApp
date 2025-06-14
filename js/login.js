3d17faeba81a272fa29d8170a038beb1b9874c35
Pecorine125 <harahellima@gmail.com>
2025-06-14 13:38:54 -0300

    asd

--- js/login.js
+++ js/login.js
@@ -1,7 +1,15 @@
-console.log("login.js carregado");
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-const form = document.getElementById('loginForm');
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  alert('Formulário enviado!\nE-mail: ' + form.email.value);
+  const email = document.getElementById('email').value.trim().toLowerCase();
+  const senha = document.getElementById('senha').value.trim();
+
+  const emailsValidos = ['programadorjunior25@gmail.com', 'harahelguilherme@gmail.com']; // ajuste emails
+  const senhasValidas = ['@HAra25082003*', '@Aquaamrine2023*', '@AiYoshino2023*', '@Rubyhoshino2023*'];
+
+  if (emailsValidos.includes(email) && senhasValidas.includes(senha)) {
+    window.location.href = 'menu.html';
+  } else {
+    alert('Email ou senha incorretos.');
+  }
 });
